0824cd4d5c65c980d48b2ac742d72807d40587b5
Abhishek <abhishekkanichery@gmail.com>
2026-07-01 13:41:16 +0530

    chore: connect tailwindcss

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
   return (
     <>
-      <div>Welcome...</div>
+      <div>
+        <h1 className="text-green-800 text-8xl font-bold">
+          Welcome to GPT BOT
+        </h1>
+      </div>
     </>
   );
 }
