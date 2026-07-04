b32767bf516300abcb1ceba264843d9523e337fa
Abhishek <abhishekkanichery@gmail.com>
2026-07-04 12:23:29 +0530

    feat: chat

--- apps/web/src/components/chat/ChatArea.tsx
+++ apps/web/src/components/chat/ChatArea.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import Header from "./Header/Header";
 import EmptyState from "./Message/EmptyState";
 
 const ChatArea = () => {
+  const [emptyState, setEmptyState] = useState(true);
   return (
     <section className="flex flex-col h-full bg-background">
       <Header />
 
       <main className="flex-1 overflow-y-auto">
-        <EmptyState />
+        {emptyState ? (
+          <EmptyState />
+        ) : (
+          <div>
+            <h1>Hi</h1>
+          </div>
+        )}
       </main>
     </section>
   );
