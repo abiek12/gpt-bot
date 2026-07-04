6db52c1121ae41cab859cf73d0c81028357e69c9
Abhishek <abhishekkanichery@gmail.com>
2026-07-04 19:41:47 +0530

    feat: update chat section

--- apps/web/src/components/chat/Message/MessageList.tsx
+++ apps/web/src/components/chat/Message/MessageList.tsx
@@ -2,12 +2,18 @@ import ChatInput from "../Input/ChatInput";
 
 const MessageList = () => {
   return (
-    <div className="container mx-auto max-w-3xl h-full flex flex-col justify-between">
+    <div className="container mx-auto max-w-3xl h-full flex flex-col justify-between py-4">
       {/* Message container */}
-      <div className="message-container">
-        <p>Hello how are you </p>
+      <div className="message-container flex flex-col gap-16">
+        {/* My message */}
+        <p className="bg-input p-4 px-6 rounded-3xl ml-auto max-w-fit">
+          Hello how are you ?
+        </p>
 
-        <p>Hello how are you </p>
+        {/* Agent message */}
+        <p className="mr-auto max-w-fit">
+          Hey there! Iam good, How can i help you ?
+        </p>
       </div>
 
       {/* Input container */}
